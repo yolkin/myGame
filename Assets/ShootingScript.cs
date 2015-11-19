e3abf7744f5fec15d3b5aff5e4e7ed34a20855e8
Alexander <m2t.alexander@gmail.com>
2015-11-19 15:05:09 +0300

    Shooting Kitten

---
+++ Assets/ShootingScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingScript : MonoBehaviour {
+	public Rigidbody projectile;
+	public float speed = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Rigidbody clone;
+			clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+			clone.velocity = transform.TransformDirection(Vector3.forward * speed);
+
+			Destroy(clone.gameObject, 3);
+		}
+	}
+}
