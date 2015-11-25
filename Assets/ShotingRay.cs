a03908848a1597b3dc812466cafe3244b622686d
Daria <bogdanovadaria20@gmail.com>
2015-11-25 23:30:59 +0300

    UI score

--- Assets/ShotingRay.cs
+++ Assets/ShotingRay.cs
@@ -8,6 +8,11 @@ public class ShotingRay : MonoBehaviour {
 	public GameObject shit;
 	public Rigidbody tennisBall;
 	public Rigidbody tennisBallGravity;
+	public AudioSource shot;
+	public Text djokovic;
+	public int djokovicScore = 0;
+	public Text nadal;
+	public int nadalScore = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -25,24 +30,31 @@ public class ShotingRay : MonoBehaviour {
 			Debug.DrawRay(transform.position, transform.forward, Color.red, 3);
 
 
-			if((hit.collider != null) && (hit.collider.tag == "Jokovic")) {
+			if((hit.collider != null) && (hit.collider.tag == "Djokovic")) {
 				GameObject ShitClone = (GameObject)Instantiate(shit, transform.position, transform.rotation);
+				shot.Play();
 				float distance = Vector3.Distance(hit.collider.transform.position, transform.position);
 				Destroy(ShitClone, distance / 15);
 				Destroy(hit.collider.gameObject, distance / 15);
+				djokovicScore++;
+				djokovic.text = "Djokovic: " + djokovicScore;
 			}
 	
 
-			if((hit.collider != null) && (hit.collider.tag == "Target")) {
+			if((hit.collider != null) && (hit.collider.tag == "Nadal")) {
 				GameObject tennisBallClone = (GameObject)Instantiate(tennisBall.gameObject, transform.position, transform.rotation);
+				shot.Play ();
 				float distance = Vector3.Distance(hit.collider.transform.position, transform.position);
 				Destroy(tennisBallClone, distance / 15);
 				Destroy(hit.collider.gameObject, distance / 15);
+				nadalScore++;
+				nadal.text = "Nadal: " + nadalScore;
 			}
 
 			if(!Physics.Raycast(ray, out hit)) {
 				Rigidbody clone = Instantiate(tennisBallGravity, transform.position, transform.rotation) as Rigidbody;;
 				clone.AddForce(transform.forward * 700);
+				shot.Play();
 				Destroy(clone.gameObject, 5);
 			}
 		}
--- Assets/Targets.cs
+++ Assets/Targets.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Targets : MonoBehaviour {
 
-	public Transform Sphere;
+	public Transform SphereNadal;
+	public Transform SphereDjokovic;
 	public int TargetNumber = 0;
 	// Use this for initialization
 	void Start () {
@@ -13,10 +14,15 @@ public class Targets : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		if (TargetNumber <= 15) {
+		if (TargetNumber < 15) {
 			TargetNumber++;
-			Vector3 randVec = new Vector3 (Random.Range (-20, 20), 0, Random.Range (-20, 20));
-			Instantiate (Sphere, randVec, new Quaternion (0, 0, 0, 0));
+			Vector3 randVec = new Vector3 (Random.Range (-10, 10), 0, Random.Range (-10, 10));
+			Instantiate (SphereNadal, randVec, new Quaternion (0, 0, 0, 0));
+		}
+		if (TargetNumber < 15) {
+			TargetNumber++;
+			Vector3 randVec = new Vector3 (Random.Range (-10, 10), 0, Random.Range (-10, 10));
+			Instantiate (SphereDjokovic, randVec, new Quaternion (0, 0, 0, 0));
 		}
 	}
 }
