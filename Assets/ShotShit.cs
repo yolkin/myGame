4c05b272e873403c116c2c38b3ff3985fa2935d2
Alexander <m2t.alexander@gmail.com>
2015-11-25 20:49:11 +0300

    Raycast
    -added raycast shot

---
+++ Assets/ShotShit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotShit : MonoBehaviour {
+
+	public float shitSpeed = 15;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position += transform.forward * shitSpeed * Time.deltaTime;
+	}
+}
